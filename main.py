5e48c0555dac03052b6a3b50a5d918c5415d2899
Dinarchak <jjustdinar@gmail.com>
2025-07-22 11:13:06 +0300

    download dependecies

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from typing import Optional
+from fastapi import FastAPI, Query
+from fastapi.responses import PlainTextResponse
+import os
 
-from fastapi import FastAPI
+app = FastAPI(title="Recruto test")
 
-app = FastAPI()
+@app.get("/", response_class=PlainTextResponse)
+async def greet(
+    name: str = Query(..., description="Имя / компания"),
+    message: str = Query(..., description="Приветственное сообщение"),
+):
+    return f"Hello {name}! {message}!"
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
